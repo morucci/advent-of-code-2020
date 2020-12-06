aa0d5fba5f9e6562c4df6b8c50398e8832f1c1c2
Fabien Boucher <fboucher@redhat.com>
2020-12-06 11:55:25 +0000

    Day 6 - part 2

--- d6/d6.ml
+++ d6/d6.ml
@@ -30,7 +30,27 @@ module Day6 = struct
     |> List.map ~f:String.concat
     |> List.map ~f:keep_uniq_letter
     |> List.map ~f:List.length |> List.fold ~init:0 ~f:( + )
-    |> printf "Result: %d\n"
+    |> printf "Result part1: %d\n"
+
+  let all_c_yes_to_nq c l =
+    let uniq = keep_uniq_letter l in
+    let exp = explode l in
+    List.fold uniq ~init:[] ~f:(fun acc e ->
+        List.fold exp ~init:0 ~f:(fun acc' e' ->
+            if Char.( = ) e' e then acc' + 1 else acc')
+        :: acc)
+    |> List.filter ~f:(fun c' -> c' = c)
+    |> List.length
+
+  let part2 input =
+    List.fold ~init:[ [] ] ~f:process_line (In_channel.read_lines input)
+    |> List.map ~f:(fun l -> (List.length l, l))
+    |> List.map ~f:(fun (c, l) -> (c, String.concat l))
+    |> List.map ~f:(fun (c, l) -> all_c_yes_to_nq c l)
+    |> List.fold ~init:0 ~f:( + )
+    |> printf "Result part2: %d\n"
 end
 
-let () = Day6.part1 "./input"
+let () =
+  Day6.part1 "./input";
+  Day6.part2 "./input"
